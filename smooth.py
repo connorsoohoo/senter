346982ad4b7ff4e296bcc6d260b64afa333e259a
Connor Soohoo <csoohoo@caltech.edu>
2019-03-02 21:58:58 -0800

    added jupyter notebook to view smoothed results

--- smooth.py
+++ smooth.py
@@ -30,14 +30,14 @@ def smooth_score(score_name, df):
 if __name__ == '__main__':
     """
     Usage:
-        python3 clean.py <input.csv>
+        python3 clean.py CollegeName
 
-    Return: Creates an input_smoothed.csv file to be used in the graphical interface
+    Return: Creates an smoothed csv file to be used in the graphical interface
     """
     assert(len(sys.argv) == 2)
 
-    filename = sys.argv[1]
-    assert(".csv" in filename)
+    college_name = sys.argv[1]
+    filename = college_name + ".csv"
     df = pd.read_csv(filename, header=0, sep=u",", index_col = None, encoding="ISO-8859-1")
 
     df = smooth_score("compound", df)
@@ -45,4 +45,4 @@ if __name__ == '__main__':
     df = smooth_score("negative", df)
     df = smooth_score("neutral", df)
 
-    df.to_csv("smoothed.csv")
+    df.to_csv(college_name + "_smoothed.csv")
