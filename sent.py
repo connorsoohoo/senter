df1320d7a13b2004f4a1916fab051cff5c098304
Alvin On (Chocomunk) <alvin.zhou.on@gmail.com>
2019-03-02 20:36:34 -0800

    Added command-line input functionality to sent.py

--- sent.py
+++ sent.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division
 
 import csv
+import argparse
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,61 +13,72 @@ from datetime import datetime
 from io import open
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# --- examples -------
-sentences = []
-output = []
-fileName = 'caltech.csv'
-# first row is timestamp, second row is confession
+CALTECH_FILE = 'caltech.csv'
 
-# df = pd.read_csv(fileName, header=0, sep=u",", index_col=None)
+def plot_sentiments(filename):
+    # --- examples -------
+    sentences = []
+    output = []
+    # first row is timestamp, second row is confession
 
-# sentences = df.as_matrix
-df = pd.read_csv(fileName, header=None, sep=u",", index_col=None)
-sentences = df[1].tolist()
-timestamps = df[0].tolist()
-# with open(fileName) as f:
-#     lines = f.readlines()
-#     for line in lines:
-#         sentences.append(line.split(',', 1))
-        # print(sentences[1])
+    # df = pd.read_csv(fileName, header=0, sep=u",", index_col=None)
 
-# with open(fileName) as csvDataFile:
-#     csvReader = csv.reader(csvDataFile)
-#     for row in csvReader:
-#         sentences.append([row[0], row[1]])
-print(len(sentences))
-# print("zero is" + sentences[0])
-# print(sentences)
-# print("one is " + sentences[1])
-analyzer = SentimentIntensityAnalyzer()
-for sentence in sentences:
-    # if (len(sentence) >= 2):
-    result = []
-    result.append(sentence)
-            # print("{:-<65} {}".format(sentence[1], str(vs)))
+    # sentences = df.as_matrix
+    df = pd.read_csv(filename, header=None, sep=u",", index_col=None)
+    sentences = df[1].tolist()
+    timestamps = df[0].tolist()
+    # with open(fileName) as f:
+    #     lines = f.readlines()
+    #     for line in lines:
+    #         sentences.append(line.split(',', 1))
+            # print(sentences[1])
 
-    vs = analyzer.polarity_scores(sentence)
-    # print(str(vs))
+    # with open(fileName) as csvDataFile:
+    #     csvReader = csv.reader(csvDataFile)
+    #     for row in csvReader:
+    #         sentences.append([row[0], row[1]])
+    print(len(sentences))
+    # print("zero is" + sentences[0])
+    # print(sentences)
+    # print("one is " + sentences[1])
+    analyzer = SentimentIntensityAnalyzer()
+    for sentence in sentences:
+        # if (len(sentence) >= 2):
+        result = []
+        result.append(sentence)
+                # print("{:-<65} {}".format(sentence[1], str(vs)))
 
-    result.append(str(vs['compound']))
-    result.append(str(vs['pos']))
-    result.append(str(vs['neg']))
-    result.append(str(vs['neu']))
+        vs = analyzer.polarity_scores(sentence)
+        # print(str(vs))
+
+        result.append(str(vs['compound']))
+        result.append(str(vs['pos']))
+        result.append(str(vs['neg']))
+        result.append(str(vs['neu']))
 
 
 
-    output.append(result)
-        # print(sentence)
-        # print("{:-<65} {}".format(sentence[1], str(vs)))
+        output.append(result)
+                # print(sentence)
+                # print("{:-<65} {}".format(sentence[1], str(vs)))
 
-i = 0
-for time in timestamps:
-    output[i].append(time)
-    i += 1
+    i = 0
+    for time in timestamps:
+        output[i].append(time)
+        i += 1
     
-# print(len(output))
-# print(output)
+    # print(len(output))
+    # print(output)
 
-# res = [[1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e']]
-my_df = pd.DataFrame(output, columns = ["confession", "compound", "positive", "negative", "neutral", "timestamp"])
-my_df.to_csv('out.csv', index=False, header=True)
+    # res = [[1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e']]
+    my_df = pd.DataFrame(output, columns = ["confession", "compound", "positive", "negative", "neutral", "timestamp"])
+    my_df.to_csv('out.csv', index=False, header=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plots sentiment data from csv")
+    parser.add_argument('-i', '--input', action='store', dest='filename', type=str, required=True,
+                        help="Path to CSV input")
+    args = parser.parse_args()
+
+    plot_sentiments(args.filename)
